97164b96207c279aaa7308efc3d77f058907d085
Lmath11 <67793708+Lmath11@users.noreply.github.com>
2023-09-25 15:35:08 -0300

    Update FPD.py

--- evaluation/FPD.py
+++ evaluation/FPD.py
@@ -100,8 +100,10 @@ def calculate_frechet_distance(mu1, sigma1, mu2, sigma2, eps=1e-6):
     mu1 = np.atleast_1d(mu1)
     mu2 = np.atleast_1d(mu2)
 
-    sigma1 = np.atleast_2d(sigma1)
-    sigma2 = np.atleast_2d(sigma2)
+   """ sigma1 = np.atleast_2d(sigma1)
+    sigma2 = np.atleast_2d(sigma2)"""
+    sigma1 += eps * np.eye(sigma1.shape[0])
+    sigma2 += eps * np.eye(sigma2.shape[0])
 
     assert mu1.shape == mu2.shape, \
         'Training and test mean vectors have different lengths'
